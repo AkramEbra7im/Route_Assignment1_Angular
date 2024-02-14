92e216d5b2cfaca3e0112909004a11ddf8d3c145
AkramEbra7im <akramebra71m@gmail.com>
2024-02-14 14:13:07 +0200

    dynamic navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,4 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    let scrollOffset = window.scrollY  || 0;
+
+    console.log(scrollOffset)
+    if ( scrollOffset ) {
+      this.isScrolled = false;
+    } else {
+      this.isScrolled = true;
+    }
+  }
+  isScrolled: boolean = true;
 }
